e77e914a0a5e0d9d8ed9329a4abeb6780982f829
nyker510 <takamail53@gmail.com>
2019-11-30 05:34:12 +0900

    [update] OutOfFoldのスコアを出力する

--- samples/stacking.py
+++ samples/stacking.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.datasets import load_boston
-
+import os
 from vivid.core import AbstractFeature, EnsembleFeature, MergeFeature
 from vivid.featureset import AbstractAtom
 from vivid.featureset.encodings import CountEncodingAtom
@@ -42,7 +42,8 @@ def main():
         *create_boosting_seed_blocks(feature_class=LGBMRegressorOutOfFold, prefix='lgbm_', parent=entry)
         # lgbm averaging models
     ]
-    mreged_feature = MergeFeature([*singles, entry], root_dir=entry.root_dir)  # 一段目のモデル + もとのデータを merge した特徴量
+    mreged_feature = MergeFeature([*singles, entry], root_dir=entry.root_dir,
+                                  name='signle_merge')  # 一段目のモデル + もとのデータを merge した特徴量
     stackings = [
         RidgeOutOfFold(name='stacking_ridge', parent=mreged_feature, n_trials=10),
         OptunaXGBRegressionOutOfFold(name='stacking_xgb', parent=mreged_feature, n_trials=10),
@@ -51,13 +52,18 @@ def main():
     stackings.append(ens)
 
     df = pd.DataFrame()
-    for feat in stackings:
+    for feat in [*stackings, *singles]:
         f_df = feat.fit(df_x, y)
         df = pd.concat([df, f_df], axis=1)
 
+    score_df = pd.DataFrame()
     for i, cols in df.T.iterrows():
-        score = regression_metrics(y, cols.values)
-        print(cols.name, score)
+        df_i = regression_metrics(y, cols.values)
+        score_df[i] = df_i['score']
+
+    score_df = score_df.T.sort_values('rmse')
+    score_df.to_csv(os.path.join(entry.root_dir, 'score.csv'))
+    print(score_df)
 
 
 if __name__ == '__main__':
