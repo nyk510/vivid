b8c9fdb6204b8797a0b0201d3650a22094ae9d94
nyker510 <takamail53@gmail.com>
2020-05-11 22:13:03 +0900

    [update] rewrite test using pytest fixture

--- tests/test_out_of_fold/conftest.py
+++ tests/test_out_of_fold/conftest.py
@@ -1,11 +1,25 @@
+import numpy as np
+import pandas as pd
 import pytest
+from sklearn.datasets import load_boston, load_breast_cancer
 
 from vivid.env import Settings
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def stop_logging():
     before = Settings.LOG_LEVEL
     Settings.LOG_LEVEL = 'WARNING'
     yield
     Settings.LOG_LEVEL = before
+
+
+@pytest.fixture
+def regression_data() -> [pd.DataFrame, np.ndarray]:
+    X, y = load_boston(True)
+    return pd.DataFrame(X), y
+
+@pytest.fixture
+def binary_data() -> [pd.DataFrame, np.ndarray]:
+    x, y = load_breast_cancer(True)
+    return pd.DataFrame(x), y
--- tests/test_out_of_fold/test_linear.py
+++ tests/test_out_of_fold/test_linear.py
@@ -1,17 +1,13 @@
 from vivid.out_of_fold.linear import RidgeOutOfFold, LogisticOutOfFold
-from .test_base import get_binary
 
 
-class TestLinear:
-    def setup_method(self):
-        df, y = get_binary()
-        self.df = df
-        self.y = y
+def test_ridge(regression_data):
+    df, y = regression_data
+    oof = RidgeOutOfFold(name='test_ridge', n_trials=10)
+    oof.fit(df, y)
 
-    def test_ridge(self):
-        oof = RidgeOutOfFold(name='test_ridge', n_trials=10)
-        oof.fit(self.df, self.y)
 
-    def test_logistic(self):
-        oof = LogisticOutOfFold(name='test_logistic', n_trials=10)
-        oof.fit(self.df, self.y)
+def test_logistic(binary_data):
+    df, y = binary_data
+    oof = LogisticOutOfFold(name='test_logistic', n_trials=10)
+    oof.fit(df, y)
