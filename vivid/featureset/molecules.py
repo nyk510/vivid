dd10ebab3f815f5a539ed2da66339db8dec69c80
nyker510 <takamail53@gmail.com>
2020-05-13 23:28:07 +0900

    [bug] import joblib directly
    * import module from another module is not recommended style

--- vivid/featureset/molecules.py
+++ vivid/featureset/molecules.py
@@ -1,8 +1,8 @@
 import os
 from typing import List
 
+import joblib
 import pandas as pd
-from sklearn.externals import joblib
 
 from vivid.core import AbstractFeature
 from vivid.utils import timer
