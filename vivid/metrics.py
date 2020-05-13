2e0084c61d7ba867e5bc9f8a7ceeab1181a8569d
nyker510 <takamail53@gmail.com>
2020-05-13 23:26:19 +0900

    [update] rm upper accuracy / ja -> english docs

--- vivid/metrics.py
+++ vivid/metrics.py
@@ -1,8 +1,5 @@
-# coding: utf-8
-"""よく使う評価指標を計算する関数などを定義する
+"""define utility functions that calculate commonly use metrics
 """
-
-from collections import OrderedDict
 
 import numpy as np
 import pandas as pd
@@ -12,6 +9,18 @@ from sklearn.metrics import roc_auc_score, log_loss, accuracy_score, f1_score, m
 
 
 def quadratic_weighted_kappa(y_true, y_pred):
+    """
+    QWK (Quadratic Weighted Kappa) Score
+
+    Args:
+        y_true:
+            target array.
+        y_pred:
+            predict array. must be a discrete format.
+
+    Returns:
+        QWK score
+    """
     return cohen_kappa_score(y_true, y_pred, weights='quadratic')
 
 
@@ -23,17 +32,19 @@ def root_mean_squared_error(y_true, y_pred,
                               multioutput=multioutput, squared=squared) ** .5
 
 
-def binary_metrics(y_true, predict_probability, threshold=.5) -> pd.DataFrame:
+def binary_metrics(y_true: np.ndarray, predict_probability: np.ndarray, threshold=.5) -> pd.DataFrame:
     """
-    二値分類でよく使う評価指標全部入りの DataFrame を作成するメソッド
+    calculate binary task metrics
+
     Args:
-        y_true(np.ndarray): 正解ラベルの配列. shape = (n_samples, )
-        predict_probability(np.ndarray): 予測値の確率が入った配列. shape = (n_samples, )
-        threshold(float): この値を超えたものをラベル 1 を予測したとみなす
-
+        y_true:
+            target. shape = (n_data,)
+        predict_probability:
+            predict value. be probability prediction for log_loss, roc_auc, etc.
+        threshold:
+            Thresholds for calculating the metrics that need to be evaluated as labels
     Returns:
-        pd.DataFrame
-
+        metrics dataframe
     """
     predict_label = np.where(predict_probability > threshold, 1, 0)
     none_prob_functions = [
@@ -63,13 +74,26 @@ def binary_metrics(y_true, predict_probability, threshold=.5) -> pd.DataFrame:
 
 
 def regression_metrics(y_true, predict) -> pd.DataFrame:
-    name_func_map = OrderedDict({
-        'rmse': lambda *x: mean_squared_error(*x) ** .5
-    })
+    """
+    calculate regression task metrics
 
-    for m in [mean_squared_log_error, median_absolute_error, median_absolute_error, mean_squared_error, r2_score,
-              explained_variance_score, mean_absolute_error]:
-        name_func_map[m.__name__] = m
+    Args:
+        y_true:
+            target. shape = (n_data,)
+        predict:
+            predict value. shape = (n_data,)
+
+    Returns:
+        regression metrics dataframe
+    """
+    name_func_map = {
+        'rmsle': lambda *x: mean_squared_log_error(*x) ** .5,
+        'rmse': root_mean_squared_error,
+        'mean-ae': mean_absolute_error,
+        'median-ae': median_absolute_error,
+        'r2': r2_score,
+        'explained_variance': explained_variance_score,
+    }
 
     data, idx = [], []
 
@@ -90,41 +114,3 @@ def regression_metrics(y_true, predict) -> pd.DataFrame:
 
     df_metrics = pd.DataFrame(data, index=idx, columns=['score'])
     return df_metrics
-
-
-def upper_accuracy(y_true, predict, n_cut=20):
-    """
-    upper accuracy を算出する
-
-    upper accuracy とは予測値をある閾値 `threshold` で区切り, 上位集合のうち
-    実際に `label = 1` のものがどの程度存在しているかを計算したものです。
-    予測値が大きいほど実際にラベルも1になってほしいので, 横軸にしきい値縦軸にその集合での label = 1 の割合 `accuracy` をプロットしたときに
-    単調増加する予測は良い予測と言えます.
-
-    Args:
-        y_true(np.ndarray): shape = (n_samples,)
-        predict:
-
-    Returns:
-        pd.DataFrame:
-            しきい値ごとの正答率のデータ.
-            カラムは `['threshold', 'count', 'hist', 'accuracy', 'ratio']` の順
-            * threshold: predict を区切った際のしきい値
-            * count: しきい値を超えたデータ点の数. int
-            * hit: しきい値を超えたデータのうち実際に y_true = 1 が満たされた点の数. int
-            * accuracy: しきい値を超えたデータのうち y_true = 1 の割合. hit / count と同値
-            * ratio: しきい値が全体の何割で区切られたかを表す数値. float
-    """
-    df_metric = pd.DataFrame([predict, y_true], index=['predict', 'label']).T
-    predict = df_metric['predict']
-    data = []
-    for r in np.linspace(0, 1, n_cut + 1)[:-1]:
-        threshold = predict.quantile(r)
-        df_i = df_metric[predict > threshold]
-        count = len(df_i)
-        hit = df_i['label'].sum()
-        acc = hit / count
-        data.append([threshold, count, hit, acc, r])
-
-    df_upper_acc = pd.DataFrame(data, columns=['threshold', 'count', 'hist', 'accuracy', 'ratio'])
-    return df_upper_acc
