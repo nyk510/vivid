0cb44f793929ffb85b5b18e5ad6c8e56b53fc018
nyker510 <takamail53@gmail.com>
2020-05-13 23:25:48 +0900

    [update] rewrite comments and add example code in docsting

--- vivid/optimizers.py
+++ vivid/optimizers.py
@@ -15,23 +15,29 @@ def get_bins(thresholds):
 
 
 class BinsOptimizer:
-    """しきい値を最適化する class
+    """Optimization class for indicators that need to be converted from continuous values to discrete values.
 
-    正解ラベル `y_true`の unique は 0 始まりのintかつ連続であると想定しています.
-    それ以外のものを与えると間違ったしきい値を返す可能性が高いため注意して下さい
+    It is assumed that the unique of the correct label `y_true` is int and continuous with a beginning of 0.
+    Note that giving anything else is likely to return an incorrect threshold value
     """
 
-    def __init__(self, objective, bins=None, minimize=True):
+    def __init__(self, objective, bins=None, minimize=True, method='Nelder-Mead'):
         """
-
         Args:
-            objective: 目的関数
-            bins: default のしきい値
-            minimize: True の時最小化問題として取り扱います
+            objective:
+                the objective function. The function should return a number
+                with y_true and a discretized predicted value as input.
+            bins:
+                threshold values. This is the initial value of the optimization solver.
+            minimize:
+                treats it as a minimization problem when true
+            method:
+                optimization method. pass to scipy minimize.
         """
         self.bins = bins
         self.objective = objective
         self.minimize = minimize
+        self.method = method
         self.result_ = None
 
     @property
@@ -41,7 +47,36 @@ class BinsOptimizer:
         t = self.result_.get('x', None)
         return get_bins(t)
 
-    def fit(self, y_true, y_pred):
+    def fit(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        fit thresholds
+
+        Args:
+            y_true:
+                target array.
+            y_pred:
+                predict array. it is the array as a continuous format
+
+        Returns:
+            optimized thresholds
+
+        Examples:
+            >>> from vivid.metrics import quadratic_weighted_kappa
+            >>> optim = BinsOptimizer(quadratic_weighted_kappa, minimize=False)
+            >>> y_pred = np.random.uniform(size=1000)
+            >>> y_true = np.where(y_pred < .5, 0, 1)
+            >>> y_pred = y_pred * .5
+            >>> before = quadratic_weighted_kappa(y_true, np.round(y_pred))
+            >>> before
+            0.0
+            >>> optim.fit(y_true, y_pred)
+            [-inf, 0.24999999999999978, inf]
+            >>> b = optim.predict(y_pred)
+            >>> after = quadratic_weighted_kappa(y_true, b)
+            >>> after
+            1.0
+        """
+
         def fnc(thresholds):
             bins = get_bins(thresholds)
             b_pred = pd.cut(y_pred, bins).codes
@@ -52,7 +87,7 @@ class BinsOptimizer:
 
         if self.bins is None:
             self.bins = thresholds_from_y_true(y_true)
-        self.result_ = minimize(fnc, self.bins, method='Nelder-Mead')
+        self.result_ = minimize(fnc, self.bins, method=self.method)
         return self.optimized_bins
 
     def predict(self, y_continuous):
