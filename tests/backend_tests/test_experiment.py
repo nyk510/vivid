409ec967a9912a58657e8f8e328c56590f90b7d9
nyker510 <takamail53@gmail.com>
2020-05-22 22:17:43 +0900

    [add] experiment backend test
    まだ不十分

---
+++ tests/backend_tests/test_experiment.py
@@ -0,0 +1,38 @@
+import os
+
+import pytest
+
+from vivid.backends.experiments import LocalExperimentBackend, ExperimentBackend
+
+
+@pytest.fixture
+def backend():
+    return [ExperimentBackend(), LocalExperimentBackend()]
+
+
+def test_can_call_method(backend: ExperimentBackend):
+    backend.save_object('foo', {})
+    backend.save_as_python_object('foo', {})
+
+    assert not backend.can_save, backend.namespace
+    assert backend.get_marked() is None
+
+
+def test_set_silent(backend: ExperimentBackend):
+    with backend.silent():
+        assert backend.logger.disabled == True
+
+
+def test_local_mark(tmpdir):
+    experiment = LocalExperimentBackend(tmpdir)
+    assert experiment.has_output_dir
+
+    obj = {
+        'bar': [1, 2]
+    }
+    experiment.mark('foo', obj)
+
+    marking = experiment.get_marked()
+
+    assert marking.get('foo') == obj
+    assert os.path.exists(tmpdir)
