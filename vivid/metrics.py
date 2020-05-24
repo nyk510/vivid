203aa8b9bab5b0cbe103e8e6278a86b6a5f66fa9
nyker510 <takamail53@gmail.com>
2020-05-24 19:05:22 +0900

    [update] define regression metrics outer method

--- vivid/metrics.py
+++ vivid/metrics.py
@@ -2,7 +2,6 @@
 """
 
 import numpy as np
-import pandas as pd
 from sklearn.metrics import roc_auc_score, log_loss, accuracy_score, f1_score, mean_absolute_error, mean_squared_error, \
     r2_score, mean_squared_log_error, median_absolute_error, explained_variance_score, cohen_kappa_score, \
     average_precision_score, precision_score, recall_score
@@ -32,7 +31,17 @@ def root_mean_squared_error(y_true, y_pred,
                               multioutput=multioutput, squared=squared) ** .5
 
 
-def binary_metrics(y_true: np.ndarray, predict_probability: np.ndarray, threshold=.5) -> pd.DataFrame:
+REGRESSION_METRICS = {
+    'rmsle': lambda *x: mean_squared_log_error(*x) ** .5,
+    'rmse': root_mean_squared_error,
+    'mean-ae': mean_absolute_error,
+    'median-ae': median_absolute_error,
+    'r2': r2_score,
+    'explained_variance': explained_variance_score,
+}
+
+
+def binary_metrics(y_true: np.ndarray, predict_probability: np.ndarray, threshold=.5) -> dict:
     """
     calculate binary task metrics
 
@@ -44,7 +53,7 @@ def binary_metrics(y_true: np.ndarray, predict_probability: np.ndarray, threshol
         threshold:
             Thresholds for calculating the metrics that need to be evaluated as labels
     Returns:
-        metrics dataframe
+        metrics series
     """
     predict_label = np.where(predict_probability > threshold, 1, 0)
     none_prob_functions = [
@@ -60,57 +69,37 @@ def binary_metrics(y_true: np.ndarray, predict_probability: np.ndarray, threshol
         average_precision_score
     ]
 
-    scores, indexes = [], []
+    scores = {}
     for f in none_prob_functions:
         score = f(y_true, predict_label)
-        scores.append(score)
-        indexes.append(f.__name__)
+        scores[str(f.__name__)] = score
     for f in prob_functions:
         score = f(y_true, predict_probability)
-        scores.append(score)
-        indexes.append(f.__name__)
+        scores[f.__name__] = score
 
-    return pd.DataFrame(data=scores, index=indexes, columns=['score'])
+    return scores
 
 
-def regression_metrics(y_true, predict) -> pd.DataFrame:
+def regression_metrics(y_true, y_pred) -> dict:
     """
     calculate regression task metrics
 
     Args:
         y_true:
             target. shape = (n_data,)
-        predict:
+        y_pred:
             predict value. shape = (n_data,)
 
     Returns:
         regression metrics dataframe
     """
-    name_func_map = {
-        'rmsle': lambda *x: mean_squared_log_error(*x) ** .5,
-        'rmse': root_mean_squared_error,
-        'mean-ae': mean_absolute_error,
-        'median-ae': median_absolute_error,
-        'r2': r2_score,
-        'explained_variance': explained_variance_score,
-    }
 
-    data, idx = [], []
+    scores = {}
 
-    for name in name_func_map.keys():
-        idx.append(name)
-
+    for k, func in REGRESSION_METRICS.items():
         try:
-            f = name_func_map[name]
-            p = np.copy(predict)
+            scores[k] = func(y_true, y_pred)
+        except Exception as e:
+            scores[k] = None
 
-            if f == mean_squared_log_error:
-                p[p < .0] = 0.
-
-            m = name_func_map[name](y_true, p)
-        except Exception:
-            m = None
-        data.append(m)
-
-    df_metrics = pd.DataFrame(data, index=idx, columns=['score'])
-    return df_metrics
+    return scores
