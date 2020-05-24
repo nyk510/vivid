74f3fe23a0361248798ff6c6b2b3e0f5381947b6
nyker510 <takamail53@gmail.com>
2020-05-24 19:35:22 +0900

    [update] no output

--- examples/001__simple.py
+++ examples/001__simple.py
@@ -1,21 +1,13 @@
 import pandas as pd
 from sklearn.datasets import load_boston
 
-from vivid.backends.experiments import LocalExperimentBackend
 from vivid.estimators.boosting import XGBRegressorBlock
 
 if __name__ == '__main__':
-    xgb = XGBRegressorBlock(name='xgb')
-
     X, y = load_boston(return_X_y=True)
     train_df = pd.DataFrame(X)
-    exp = LocalExperimentBackend(namespace='./outputs/simple')
-    exp.clear()
-    xgb.fit(train_df, y, experiment=exp)
 
-    # re-train is no time (use cache)
-    xgb.fit(train_df, y, experiment=exp)
+    xgb = XGBRegressorBlock('xgb')
+    xgb.fit(train_df, y)
 
-    # switch to other environment, no longer use cache
-    with exp.as_environment('new_env', style='nested') as new_exp:
-        xgb.fit(train_df, y, experiment=new_exp)
+    xgb.predict(train_df)
