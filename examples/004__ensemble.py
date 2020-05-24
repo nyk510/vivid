a893dbd4db789a46917fb7a4ae5d3c43b448f4b7
nyker510 <takamail53@gmail.com>
2020-05-24 19:35:37 +0900

    [update] more complex

--- examples/004__ensemble.py
+++ examples/004__ensemble.py
@@ -8,6 +8,7 @@ from vivid.estimators.boosting import XGBRegressorBlock
 from vivid.estimators.ensumble import RFRegressorBlock
 from vivid.estimators.linear import TunedRidgeBlock
 from vivid.estimators.neural_network import KerasRegressorBlock
+from vivid.estimators.svm import SVRBlock
 from vivid.features.blocks import BinningCountBlock, FilterBlock
 
 
@@ -29,19 +30,33 @@ if __name__ == '__main__':
         XGBRegressorBlock(name='xgb', parent=features),
         TunedRidgeBlock('ridge', parent=features),
         RFRegressorBlock('rf', parent=features),
-        KerasRegressorBlock('keras', parent=features)
+        KerasRegressorBlock('keras', parent=features),
+        SVRBlock('svr', parent=features)
     ]
 
     ensemble = EnsembleBlock('ens', parent=models)
+    stacking = [
+        SVRBlock('svr', parent=models),
+        TunedRidgeBlock('ridge', parent=models)
+    ]
+    stacked_stack = TunedRidgeBlock('ridge', parent=[*stacking, *models, *features])
 
-    print(ensemble.show_network())
+    print(stacked_stack.show_network())
     X, y = load_boston(return_X_y=True)
     train_df = pd.DataFrame(X)
 
     experiment = LocalExperimentBackend(namespace='./outputs/ensemble')
 
-    ensemble.fit(train_df, y, experiment=experiment)
-    ensemble.predict(train_df, experiment=experiment)
+    stacked_stack.fit(train_df, y, experiment=experiment)
+    stacked_stack.predict(train_df, experiment=experiment, ignore_storage=True)
 
     other_model = XGBRegressorBlock('xgb2', parent=features)
     other_model.fit(train_df, y, experiment=experiment)
+
+    # ensemble model already fitted,
+    ensemble.fit(train_df, y, experiment=experiment)
+
+    estimators = set([b for b in stacked_stack.all_network_blocks() if b.is_estimator])
+    for b in estimators:
+        print('predict {}'.format(b.name))
+        b.predict(train_df)
