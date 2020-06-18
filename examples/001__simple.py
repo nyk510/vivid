30c648d665b67d516628314855d697c3ff07b0a6
nyker510 <takamail53@gmail.com>
2020-06-19 07:32:17 +0900

    [update] add classifier sample

--- examples/001__simple.py
+++ examples/001__simple.py
@@ -1,17 +1,22 @@
+import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston
 
 from vivid.backends.experiments import LocalExperimentBackend
-from vivid.estimators.boosting import XGBRegressorBlock
+from vivid.estimators.boosting import XGBRegressorBlock, XGBClassifierBlock
 
 if __name__ == '__main__':
     X, y = load_boston(return_X_y=True)
+    y_classifier = np.where(y > 10., 1, 0)
     train_df = pd.DataFrame(X)
-
-    xgb = XGBRegressorBlock('xgb')
-
     experiment = LocalExperimentBackend('./outputs/simple')
 
+    xgb = XGBRegressorBlock('xgb_reg')
     with experiment.as_environment(xgb.runtime_env) as exp:
         oof = xgb.fit(train_df, y, experiment=exp)
         xgb.report(train_df, oof, y, experiment=exp)
+
+    xgb = XGBClassifierBlock('xgb_classifier')
+    with experiment.as_environment(xgb.runtime_env) as exp:
+        oof = xgb.fit(train_df, y_classifier, experiment=exp)
+        xgb.report(train_df, oof, y_classifier, experiment=exp)
